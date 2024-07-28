8d28366f658ce01d9ef37c569008a04d0523e86a
Liam Cottrell <lemcottrell@gmail.com>
2024-07-28 21:43:27 +0100

    update fl name

--- linda/index.php
+++ linda/index.php
@@ -1,3 +1,17 @@
 <?php
 echo "The TFL.org-approved Linda Cardellini fanlisting: coming soon!";
 ?>
+
+<!-- linda
+adjective (Spanish)
+/ˈlin.da/
+
+1. Pretty: Attractively pleasing in a delicate or charming way. 
+
+cardellini
+noun (Italian)
+/kar.delˈliː.ni/
+
+1. Goldfinches: Small passerine birds belonging to the finch family, known for their bright plumage and melodic songs.
+
+-->

01daf884f83cc7adb44c2019c6b08fc0a7c805c3
Liam Cottrell <lemcottrell@gmail.com>
2024-08-01 22:38:05 +0100

    make fl about page

--- linda/index.php
+++ linda/index.php
@@ -12,14 +12,4 @@ include ENTH_PATH . 'show_stats.php';
 
 require_once('footer.php');
 
-// linda
-// adjective (Spanish)
-// /ˈlin.da/
 
-// 1. Pretty: Attractively pleasing in a delicate or charming way. 
-
-// cardellini
-// noun (Italian)
-// /kar.delˈliː.ni/
-
-// 1. Goldfinches: Small passerine birds belonging to the finch family, known for their bright plumage and melodic songs.

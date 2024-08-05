dc343a5ca21ef5536d5ab3b981e9cff5f976df62
Liam Cottrell <lemcottrell@gmail.com>
2024-08-05 23:03:12 +0100

    edit fl

--- linda/index.php
+++ linda/index.php
@@ -4,16 +4,22 @@ require_once('header.php');
 
     <h3>Welcome...</h3>
 
-<?php
-echo "...to the TFL.org-approved Linda Cardellini fanlisting!<br><br>";
-?>
+<p>...to <em>Pretty Goldfinches</em>, the only TheFanlistings.Org-approved Fanlisting for the actor Linda Cardellini!</p>
+<p>This Fanlisting is owned by <a href="https://liamco.io/" target="_blank">Liam</a></p>
+
+
 
 
 
 <?php
 include 'config.php';
 include ENTH_PATH . 'show_stats.php';
+?>
 
+<?= '<div style="font-size: 0.8rem"><em>Disclaimer: I am not Linda Cardellini and I am not affiliated with her in any way. This is a fan-made project and no copyright infringement is intended.</em></div>'?>
+
+
+<?php
 require_once('footer.php');
 
 

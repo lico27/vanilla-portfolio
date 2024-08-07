890e6839f3c38482c37f8ba9b94ba21b4e4df76f
Liam Cottrell <lemcottrell@gmail.com>
2024-08-07 15:18:17 +0100

    add fl codes

--- linda/codes.php
+++ linda/codes.php
@@ -4,7 +4,28 @@ require_once('header.php');
 
     <h3>Codes</h3>
 
-    <?= '<p>Here you will find some codes that you can use to link back! Please host them yourself rather than direct linking :)</p>'?>
+    <?= '<p>Here you will find some codes that you can use to link back to <strong>liamco.io/linda</strong>. Please host them yourself rather than direct linking :)</p>'?>
+
+    <?= '<p style="font-weight:bold">50x50</p>'?>
+    <?= '<div style="display:flex">
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/50x50-1.png"></div>
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/50x50-2.png"></div>
+         </div>'
+    ?>
+
+    <?= '<p style="font-weight:bold; margin-top: 15px;">100x35</p>'?>
+    <?= '<div style="display:flex">
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/100x35-1.png"></div>
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/100x35-2.png"></div>
+         </div>'
+    ?>  
+
+    <?= '<p style="font-weight:bold">100x50</p>'?>
+    <?= '<div style="display:flex">
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/100x50-1.png"></div>
+         <div style="height:50px; margin-right: 5px;"><img src="./assets/img/100x50-2.png"></div>
+         </div>'
+    ?>  
 
 <?php
 require_once('footer.php');

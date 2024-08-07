41c8c46d130f8b0afde44fdf408e01c062a4ed4b
Liam Cottrell <lemcottrell@gmail.com>
2024-08-07 14:40:49 +0100

    make fl code page

--- linda/codes.php
+++ linda/codes.php
@@ -4,6 +4,7 @@ require_once('header.php');
 
     <h3>Codes</h3>
 
-<?php
+    <?= '<p>Here you will find some codes that you can use to link back! Please host them yourself rather than direct linking :)</p>'?>
 
+<?php
 require_once('footer.php');
--- linda/index.php
+++ linda/index.php
@@ -4,21 +4,16 @@ require_once('header.php');
 
     <h3>Welcome...</h3>
 
-<p>...to <em>Pretty Goldfinches</em>, the only TheFanlistings.Org-approved Fanlisting for the actor Linda Cardellini!</p>
-<p>This Fanlisting is owned by <a href="https://liamco.io/" target="_blank">Liam</a>.</p>
-
-
-
-
+    <?= '<p style="margin-bottom: 5px">...to <em>Pretty Goldfinches</em>, the only TheFanlistings.Org-approved Fanlisting for the actor Linda Cardellini!</p>'?>
+    <?= '<p style="margin-bottom: 10px">This Fanlisting is owned by <a href="https://liamco.io/" target="_blank">Liam</a>.</p>'?>
 
 <?php
 include 'config.php';
 include ENTH_PATH . 'show_stats.php';
 ?>
 
-<?= '<div style="font-size: 0.8rem; text-align: center; margin-bottom: 5px"><em>Disclaimer: I am not Linda Cardellini and I am not affiliated with her in any way. This is a fan-made project and no copyright infringement is intended.</em></div>'?>
-<?= '<div style="font-size: 0.8rem; text-align: center">Credits: <a href="https://netflixqueue.com/linda-cardellini-not-looking-back" target="blank">Image</a> | <a href="https://www.brusheezy.com/members/axeraider70" target="_blank">Brushes</a> | <a href="https://www.1001fonts.com/users/MistiFonts/" target="_blank">Font</a> and <a href="https://www.1001fonts.com/users/Gazbriel/" target="_blank">Font</a></div>'?>
-
+    <?= '<div style="font-size: 0.8rem; text-align: center; margin-bottom: 5px"><em>Disclaimer: I am not Linda Cardellini and I am not affiliated with her in any way. This is a fan-made project and no copyright infringement is intended.</em></div>'?>
+    <?= '<div style="font-size: 0.8rem; text-align: center">Credits: <a href="https://netflixqueue.com/linda-cardellini-not-looking-back" target="blank">Image</a> | <a href="https://www.brusheezy.com/members/axeraider70" target="_blank">Brushes</a> | <a href="https://www.1001fonts.com/users/MistiFonts/" target="_blank">Font</a> and <a href="https://www.1001fonts.com/users/Gazbriel/" target="_blank">Font</a></div>'?>
 
 <?php
 require_once('footer.php');
